04dca89d5a5db45f5319d1236ad627b996f28621
BATMINJI <minjiconan@naver.com>
2026-06-16 09:47:57 +0900

    Game Result 구조체, 델리게이트, 클래스 선언

--- Source/UEWebAPIStudy/RankingSubsystem.h
+++ Source/UEWebAPIStudy/RankingSubsystem.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -12,12 +12,62 @@ struct FGameResult
 {
 	GENERATED_BODY()
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	int32 Id = 0;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	FString UserId = "";
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	FString UserName = "";
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	FString UserEmail = "";
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	int32 Score = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Game Result")
+	FString FDate = "";
 };
+
+// Add, Update, Delete 결과용 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnActionSuccessSignature, bool, bSuccess);
+
+// 전체 랭킹 조회 결과용 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGetRankingListSignature, const TArray<FGameResult>&, RankingList);
+
+// 단일 랭킹 조회 결과용 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGetSingleRankingSignature, bool, bSuccess, const FGameResult&, GameResult);
 
 UCLASS()
 class UEWEBAPISTUDY_API URankingSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Ranking")
+	void AddGameResult(const FGameResult& NewResult);
+
+	UFUNCTION(BlueprintCallable, Category = "Ranking")
+	void GetGameResults();
+
+	UFUNCTION(BlueprintCallable, Category = "Ranking")
+	void GetGameResultById(int32 TargetId);
+
+	UFUNCTION(BlueprintCallable, Category = "Ranking")
+	void UpdateGameResult(int32 TargetId, const FGameResult& UpdatedResult);
+
+	UFUNCTION(BlueprintCallable, Category = "Ranking")
+	void DeleteGameResult(int32 TargetId);
+
+	// Delegates
+	UPROPERTY(BlueprintAssignable, Category = "Ranking|Delegates")
+	FOnActionSuccessSignature OnActionCompleted;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ranking|Delegates")
+	FOnGetRankingListSignature OnGetRankingListCompleted;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ranking|Delegates")
+	FOnGetSingleRankingSignature OnGetSingleRankingCompleted;
 };
